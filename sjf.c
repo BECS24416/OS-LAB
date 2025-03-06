452e1c8c132375f89ea5ad878a40478c1bacd6c8
vidya <vidyashreev.cs24@bmsce.ac.in>
2025-03-06 15:56:24 +0530

    Add files via upload

---
+++ sjf.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 10
+
+struct process {
+    int id, AT, BT, CT, TAT, WT, RT, remaining_BT;
+    int completed;
+};
+
+void sort_by_AT(struct process p[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (p[i].AT > p[j].AT) {
+                struct process temp = p[i];
+                p[i] = p[j];
+                p[j] = temp;
+            }
+        }
+    }
+}
+void calculate_SJF_NonPreemptive(struct process p[], int n) {
+    int completed = 0, currentTime = 0;
+    while (completed < n) {
+        int shortest = -1, minBT = INT_MAX;
+        for (int i = 0; i < n; i++) {
+            if (!p[i].completed && p[i].AT <= currentTime && p[i].BT < minBT) {
+                minBT = p[i].BT;
+                shortest = i;
+            }
+        }
+
+        if (shortest == -1) {
+            currentTime++;
+        } else {
+            p[shortest].RT = currentTime - p[shortest].AT;
+            p[shortest].CT = currentTime + p[shortest].BT;
+            currentTime = p[shortest].CT;
+            p[shortest].TAT = p[shortest].CT - p[shortest].AT;
+            p[shortest].WT = p[shortest].TAT - p[shortest].BT;
+            p[shortest].completed = 1;
+            completed++;
+        }
+    }
+}
+void calculate_SJF_Preemptive(struct process p[], int n) {
+    int completed = 0, currentTime = 0;
+    for (int i = 0; i < n; i++) {
+        p[i].remaining_BT = p[i].BT;
+    }
+    while (completed < n) {
+        int shortest = -1, minBT = INT_MAX;
+        for (int i = 0; i < n; i++) {
+            if (!p[i].completed && p[i].AT <= currentTime && p[i].remaining_BT < minBT) {
+                minBT = p[i].remaining_BT;
+                shortest = i;
+            }
+        }
+
+        if (shortest == -1) {
+            currentTime++;
+        } else {
+            if (p[shortest].remaining_BT == p[shortest].BT)
+                p[shortest].RT = currentTime - p[shortest].AT;
+
+            p[shortest].remaining_BT--;
+            currentTime++;
+
+            if (p[shortest].remaining_BT == 0) {
+                p[shortest].CT = currentTime;
+                p[shortest].TAT = p[shortest].CT - p[shortest].AT;
+                p[shortest].WT = p[shortest].TAT - p[shortest].BT;
+                p[shortest].completed = 1;
+                completed++;
+            }
+        }
+    }
+}
+void display(struct process p[], int n) {
+    printf("\nProcess\tAT\tBT\tCT\tTAT\tWT\tRT\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\n", p[i].id, p[i].AT, p[i].BT, p[i].CT, p[i].TAT, p[i].WT, p[i].RT);
+    }
+}
+int main (){
+   struct process p[MAX];
+int n;
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        p[i].id = i + 1;
+        printf("Enter Arrival Time (AT) for process %d: ", i + 1);
+        scanf("%d", &p[i].AT);
+        printf("Enter Burst Time (BT) for process %d: ", i + 1);
+        scanf("%d", &p[i].BT);
+        p[i].completed = 0;
+    }
+calculate_SJF_NonPreemptive(p, n);
+                display(p, n);
+calculate_SJF_Preemptive(p, n);
+                display(p, n);
+                return 0;
+}
